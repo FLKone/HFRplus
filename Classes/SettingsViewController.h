b343793b263fb9c24c3ab7e37baec6694e16653a
FLK <thefolken@online.fr>
2012-07-05 19:56:28 +0200

    Fix: Outlets

---
+++ Classes/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  HFRplus
+//
+//  Created by FLK on 05/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "IASKAppSettingsViewController.h"
+
+@interface SettingsViewController : IASKAppSettingsViewController
+
+@end
---
+++ Classes/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  HFRplus
+//
+//  Created by FLK on 05/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
