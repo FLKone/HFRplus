21567f8407b78f3d8acf231227c17fd2deba4a8c
FLKone <FLKone@users.noreply.github.com>
2015-09-16 13:26:12 +0200

    Fix: Les des messages, Le clavier n'apparaissait pas automatiquement pour saisir un numéro de page.

--- Classes/PageViewController.m
+++ Classes/PageViewController.m
@@ -43,24 +43,63 @@
 -(void)choosePage {
 	//NSLog(@"choosePage");
 	
-	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Aller à la page" message:nil
-												   delegate:self cancelButtonTitle:@"Annuler" otherButtonTitles:@"OK", nil];
-	
-    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
     
-    UITextField *textField = [alert textFieldAtIndex:0];
-    textField.placeholder = [NSString stringWithFormat:@"(numéro entre %d et %d)", [self firstPageNumber], [self lastPageNumber]];
-    textField.textAlignment = NSTextAlignmentCenter;
-    textField.delegate = self;
-    [textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
-    textField.keyboardAppearance = UIKeyboardAppearanceDefault;
-    textField.keyboardType = UIKeyboardTypeNumberPad;
     
-	[alert setTag:668];
-	[alert show];
+    if ([UIAlertController class]) {
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Aller à la page"
+                                                                       message:nil
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        
+        [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            textField.placeholder = [NSString stringWithFormat:@"(numéro entre %d et %d)", [self firstPageNumber], [self lastPageNumber]];
+            textField.textAlignment = NSTextAlignmentCenter;
+            [textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+            textField.keyboardAppearance = UIKeyboardAppearanceDefault;
+            textField.keyboardType = UIKeyboardTypeNumberPad;
+            textField.delegate = self;
+        }];
+        
+        UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Annuler" style:UIAlertActionStyleCancel
+                                                             handler:^(UIAlertAction * action) {
+                                                                 
+                                                             }];
+        
+        [alert addAction:cancelAction];
+        
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {
+                                                                  [self gotoPageNumber:[[alert.textFields[0] text] intValue]];
+                                                              }];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:^{
+            
+        }];
+        
+        
+        
+    } else {
+    
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Aller à la page" message:nil
+                                                       delegate:self cancelButtonTitle:@"Annuler" otherButtonTitles:@"OK", nil];
+        
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+        
+        UITextField *textField = [alert textFieldAtIndex:0];
+        textField.placeholder = [NSString stringWithFormat:@"(numéro entre %d et %d)", [self firstPageNumber], [self lastPageNumber]];
+        textField.textAlignment = NSTextAlignmentCenter;
+        textField.delegate = self;
+        [textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+        textField.keyboardAppearance = UIKeyboardAppearanceDefault;
+        textField.keyboardType = UIKeyboardTypeNumberPad;
+        
+        [alert setTag:668];
+        [alert show];
 
-	[alert release];
-	
+        [alert release];
+    }
 }
 
 - (void)goToPage:(NSString *)pageType;
